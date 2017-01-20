476f132aeb65cfe2e6a1c4ad29cbf9d788521d59
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-01-20 19:20:47 +0100

    Test some css grid stuff.

--- js/App.vue
+++ js/App.vue
@@ -1,14 +1,27 @@
-<template>
-  <div id="app">
-    <router-link to="/foo">Foo</router-link>
-    <router-link to="/bar">Bar</router-link>
-    <router-view class="view"></router-view>
-  </div>
-</template>
-
 <style lang="scss">
-  @import '~@avalanche/object-grid';
-  body {
-    background: red;
+  @import '{ .o-grid } from ~@avalanche/object-grid';
+
+  .u-width-4\/12 {
+    width: 33.3333333% !important;
+  }
+
+  .u-width-8\/12 {
+    width: 66.6666666% !important;
   }
 </style>
+
+<template>
+  <div id="app" class="c-app">
+    <div class="o-grid">
+      <nav class="o-grid__item u-width-4/12">
+        <ul>
+            <li><router-link to="/foo">Foo</router-link></li>
+            <li><router-link to="/bar">Bar</router-link></li>
+        </ul>
+      </nav>
+      <main class="o-grid__item u-width-8/12">
+        <router-view class="view"></router-view>
+      </main>
+    </div>
+  </div>
+</template>
