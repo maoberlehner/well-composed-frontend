cae44700a24edd2163f4a5f58a3ccae7bd50eb39
Markus Oberlehner <markus.oberlehner@karriere.at>
2017-01-24 12:25:49 +0100

    Use span instead of div for the (inline) before and after icons on the button component.

--- app/components/Button.vue
+++ app/components/Button.vue
@@ -10,12 +10,12 @@
 
 <template>
   <button class="c-button" @click="handleClickEvent">
-    <div class="c-button__icon c-button__icon--before" v-if="this.$slots['icon-before']">
+    <span class="c-button__icon c-button__icon--before" v-if="this.$slots['icon-before']">
       <slot name="icon-before"></slot>
-    </div>
+    </span>
     <slot></slot>
-    <div class="c-button__icon c-button__icon--after" v-if="this.$slots['icon-after']">
+    <span class="c-button__icon c-button__icon--after" v-if="this.$slots['icon-after']">
       <slot name="icon-after"></slot>
-    </div>
+    </span>
   </button>
 </template>
