6f92a01ce16fe6faab2ff4f455c10194d19a032a
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-01-27 17:00:36 +0100

    Optimize the styling.

--- app/App.vue
+++ app/App.vue
@@ -15,22 +15,35 @@
 </style>
 
 <style lang="scss" scoped>
-  @import '{ .o-grid } from ~@avalanche/object-grid';
+  @import '~@avalanche/setting-breakpoints';
+  @import '~@avalanche/setting-spacings';
+  @import '{
+    .o-grid,
+    .o-grid--xxl-horizontal
+  } from ~@avalanche/object-grid';
   @import '{
     .u-width-12/12,
     .u-width-3/12@m,
     .u-width-9/12@m
   } from ~@avalanche/utility-width';
+
+  .c-app {
+    padding: setting-spacing(m);
+
+    @media (min-width: setting-breakpoint(m)) {
+      padding: 0;
+    }
+  }
 </style>
 
 <template>
   <div id="app" class="c-app">
-    <div class="o-grid">
-      <div class="o-grid__item u-width-12/12 u-width-3/12@m">
+    <div class="o-grid o-grid--xxl-horizontal c-app__grid">
+      <div class="o-grid__item c-app__sidebar u-width-12/12 u-width-3/12@m">
         <main-nav></main-nav>
       </div>
-      <main class="o-grid__item u-width-12/12 u-width-9/12@m">
-        <router-view class="view"></router-view>
+      <main class="o-grid__item c-app__main u-width-12/12 u-width-9/12@m">
+        <router-view></router-view>
       </main>
     </div>
   </div>
