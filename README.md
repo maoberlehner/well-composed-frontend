beeea9e1fea78b7f23d0c54f5256b9f1ea9eb742
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-01-28 14:18:11 +0100

    Fix the link to avalanche in the about text, add a link to the github project and use the about the in the README file.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # A well-composed frontend
+This is an example app to show how to build a flexible and modular frontend. The main technologies used are [Vue](https://vuejs.org) together with [Vuex](https://vuex.vuejs.org/en/) and [webpack](https://webpack.github.io/) in combination with the [node-sass-magic-importer](https://www.npmjs.com/package/node-sass-magic-importer).
+
+The two main goals of a well-composed frontend are: a solid user experience and a solid developer experience. The former is achieved by building a fast (instantly) loading site with app like behaviour. The later is solved by building upon a solid framework and using components (custom and ready-made) to keep the code DRY.
+
+To enable (almost) instant loading, server side rendering is utilized to speed up the first view and a service worker to make it possible to load the app blazing fast on repeated views.
+
+Vue serves as a very solid framework to build mighty apps upon. Vuex is used to manage the state of the app.
+
+The combination of webpack and node-sass-magic-importer makes it possible to modularize every aspect of the frontend workflow. Frontend developers are using JavaScript modules for quite some time now and with ES6 it got even more comfortable doing so. Although SASS made it much easier to also modularize your CSS code it is still laking an easy way to load styles from third party modules. For this project node-sass-magic-importer is used to load styles from [avalanche](https://avalanche.oberlehner.net/) packages.
+
 ## About
 ### Author
 Markus Oberlehner  
--- app/views/About.vue
+++ app/views/About.vue
@@ -23,7 +23,8 @@
       <p>The two main goals of a well-composed frontend are: a solid user experience and a solid developer experience. The former is achieved by building a fast (instantly) loading site with app like behaviour. The later is solved by building upon a solid framework and using components (custom and ready-made) to keep the code DRY.</p>
       <p>To enable (almost) instant loading, server side rendering is utilized to speed up the first view and a service worker to make it possible to load the app blazing fast on repeated views.</p>
       <p>Vue serves as a very solid framework to build mighty apps upon. Vuex is used to manage the state of the app.</p>
-      <p>The combination of webpack and node-sass-magic-importer makes it possible to modularize every aspect of the frontend workflow. Frontend developers are using JavaScript modules for quite some time now and with ES6 it got even more comfortable doing so. Although SASS made it much easier to also modularize your CSS code it is still laking an easy way to load styles from third party modules. For this project node-sass-magic-importer is used to load styles from [avalanche](https://avalanche.oberlehner.net/) packages.</p>
+      <p>The combination of webpack and node-sass-magic-importer makes it possible to modularize every aspect of the frontend workflow. Frontend developers are using JavaScript modules for quite some time now and with ES6 it got even more comfortable doing so. Although SASS made it much easier to also modularize your CSS code it is still laking an easy way to load styles from third party modules. For this project node-sass-magic-importer is used to load styles from <a href="https://avalanche.oberlehner.net/">avalanche</a> packages.</p>
+      <p><a href="https://github.com/maoberlehner/well-composed-frontend">View the code on GitHub.</a></p>
     </div>
   </div>
 </template>
