40a3df7ebc6e6be31e8b61f7ba0c2bc8ffe7cea2
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-01-28 07:59:50 +0100

    Remove icon functionality from the button component because it is not used.

--- app/components/form/Button.vue
+++ app/components/form/Button.vue
@@ -29,12 +29,6 @@
 
 <template>
   <button class="c-button" @click="handleClickEvent">
-    <span class="c-button__icon c-button__icon--before" v-if="this.$slots['icon-before']">
-      <slot name="icon-before"></slot>
-    </span>
     <slot></slot>
-    <span class="c-button__icon c-button__icon--after" v-if="this.$slots['icon-after']">
-      <slot name="icon-after"></slot>
-    </span>
   </button>
 </template>
