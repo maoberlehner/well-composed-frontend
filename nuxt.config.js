cc8edebdbbd33385634501d29c25e5db80c22aa7
Markus Oberlehner <markus.oberlehner@karriere.at>
2017-06-28 16:12:55 +0200

    Better looking title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
    * Headers of the page.
    */
   head: {
-    title: `a-well-composed-frontend`,
+    title: `A well-composed frontend`,
     meta: [
       { charset: `utf-8` },
       { name: `viewport`, content: `width=device-width, initial-scale=1` },
