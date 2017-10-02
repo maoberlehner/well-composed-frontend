594217eee5995c012b36bf19b72abf0d19f991b5
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-10-02 06:03:10 +0200

    Add a workaround because selector importing is not possible with vue-loader

--- components/form/Button.vue
+++ components/form/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="c-form-button" @click="click">
+  <button class="c-button" @click="click">
     <slot></slot>
   </button>
 </template>
@@ -17,5 +17,8 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../assets/scss/settings/generic';
+// Selector importing does not work because of
+// https://github.com/vuejs/vue-loader/issues/673
+// as a workaround we're using the `c-button` class above.
 @import '{ .c-button as .c-form-button } from ~@avalanche/component-button';
 </style>
