c114f29448a47249d2bb9eea057df799a576c414
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-10-04 06:05:30 +0200

    Add missing error stati

--- components/form/Message.vue
+++ components/form/Message.vue
@@ -1,5 +1,11 @@
 <template>
-  <div :class="{ 'c-form-message': true, 'c-form-message--error': type === 'error' }">
+  <div :class="{
+      'c-form-message': true,
+      'c-form-message--success': isSuccess,
+      'c-form-message--error': isError,
+      'c-form-message--warning': isWarning,
+      'c-form-message--info': isInfo,
+    }">
     <slot></slot>
   </div>
 </template>
@@ -7,6 +13,12 @@
 <script>
 export default {
   name: `FormMessage`,
+  computed: {
+    isSuccess() { return this.type === `success`; },
+    isError() { return this.type === `error`; },
+    isWarning() { return this.type === `warning`; },
+    isInfo() { return this.type === `info`; },
+  },
   props: [
     `type`,
   ],
