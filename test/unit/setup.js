0de3732b91efcd16ebac876328028570a5c2cdbb
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-10-21 17:05:22 +0200

    Optimize comments

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,9 +1,9 @@
 const hooks = require(`require-extension-hooks`);
 
-// Setup browser environment.
+// Set up a virtual browser environment.
 require(`jsdom-global`)();
 
-// Setup vue files to be processed by `require-extension-hooks-vue`.
+// Setup `.vue` files to be processed by `require-extension-hooks-vue`.
 hooks(`vue`).plugin(`vue`).push();
-// Setup vue and js files to be processed by `require-extension-hooks-babel`.
+// Setup `.vue` and `.js` files to be processed by `require-extension-hooks-babel`.
 hooks([`vue`, `js`]).plugin(`babel`, { plugins: [`transform-object-rest-spread`] }).push();
