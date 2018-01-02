87a36b4c0fac42af118fbaebbdb5c150688fddc1
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-01-02 06:38:24 +0100

    Do not throw errors in actions
    See #23

--- store/modules/post.js
+++ store/modules/post.js
@@ -2,17 +2,11 @@ export function makePost({ getPost, listPosts, types }) {
   const actions = {
     fetchPosts({ commit }) {
       return listPosts()
-        .then(response => commit(types.SET_POSTS, response.data.slice(-20)))
-        .catch((error) => {
-          throw new Error(error);
-        });
+        .then(response => commit(types.SET_POSTS, response.data.slice(-20)));
     },
     fetchPost({ commit }, id) {
       return getPost(id)
-        .then(response => commit(types.SET_POST, response.data))
-        .catch((error) => {
-          throw new Error(error);
-        });
+        .then(response => commit(types.SET_POST, response.data));
     },
   };
 
