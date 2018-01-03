be1d85a4bcfe2c862042e52a5e9c86e86f9a80a3
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-01-03 15:14:32 +0100

    Remove obsolete comment and use renamed CSS class

--- components/atoms/app/AppButton.vue
+++ components/atoms/app/AppButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="c-button">
+  <button class="c-app-button">
     <slot></slot>
   </button>
 </template>
@@ -12,8 +12,5 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../../assets/scss/settings/generic';
-// Selector importing does not work because of
-// https://github.com/vuejs/vue-loader/issues/673
-// as a workaround we're using the `c-button` class above.
 @import '{ .c-button as .c-app-button } from ~@avalanche/component-button';
 </style>
