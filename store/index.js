c2e3e3378503fe0193aa2cc555c6dd5c168ead83
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-06-15 16:52:01 +0200

    Cleanup the store and removed unused files

--- store/index.js
+++ store/index.js
@@ -1,15 +1,3 @@
 import Vuex from 'vuex';
 
-import actions from './actions';
-import getters from './getters';
-import mutations from './mutations';
-import state from './state';
-
-const createStore = () => new Vuex.Store({
-  actions,
-  getters,
-  mutations,
-  state,
-});
-
-export default createStore;
+export default () => new Vuex.Store();
