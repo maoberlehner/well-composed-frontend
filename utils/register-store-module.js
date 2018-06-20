182725d3e20c1ccf594407381dccca541608e43a
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-06-20 17:25:58 +0200

    Fix module name in register function

--- utils/register-store-module.js
+++ utils/register-store-module.js
@@ -3,5 +3,5 @@ export default function registerStoreModule({ module, moduleName, store }) {
   const moduleIsRegistered = store._modules.root._children[moduleName] !== undefined;
   const stateExists = store.state[moduleName];
 
-  if (!moduleIsRegistered) store.registerModule(`post`, module, { preserveState: stateExists });
+  if (!moduleIsRegistered) store.registerModule(moduleName, module, { preserveState: stateExists });
 }
