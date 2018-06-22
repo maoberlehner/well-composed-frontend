4898e66b6cc9bd2641d04f72867c58dafdd3a5f6
Markus Oberlehner <markus.oberlehner@karriere.at>
2018-06-22 10:35:00 +0200

    Make variable strictly a boolean

--- utils/register-store-module.js
+++ utils/register-store-module.js
@@ -1,7 +1,7 @@
 export default function registerStoreModule({ module, moduleName, store }) {
   // eslint-disable-next-line no-underscore-dangle
   const moduleIsRegistered = store._modules.root._children[moduleName] !== undefined;
-  const stateExists = store.state[moduleName];
+  const stateExists = store.state[moduleName] !== undefined;
 
   if (!moduleIsRegistered) store.registerModule(moduleName, module, { preserveState: stateExists });
 }
